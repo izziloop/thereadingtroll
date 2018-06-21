f50a80313e9ec74226de291f0e1f8afcae4c5c11
Justine Rochas <justine.rochas@activeeon.com>
2018-06-21 10:58:51 +0200

    Add project structure

---
+++ src/main/java/com/izziloop/thereadingtroll/thereadingtroll/ThereadingtrollApplication.java
@@ -0,0 +1,12 @@
+package com.izziloop.thereadingtroll.thereadingtroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThereadingtrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThereadingtrollApplication.class, args);
+	}
+}
